3cec4d697cbeda27bed8b3893623a545f60ff353
Pozolitoxd <estebantoro.p.7@gmail.com>
2025-05-18 10:58:18 -0500

    Completando la ruta quieroSumar

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //import {sumar, restar} from './OperacionesEnteros/enteros';
-const calculadora = require('./OperacionesEnteros/enteros');
+const calculadora = require('./OperacionesEnteros/enteros.js');
 const express = require('express');
 
 const app = express ();
 
-app.get ('/quieroSumar', (res, req))
+app.get ('/quieroSumar', (res, req) => {
+    res.end ('<h2> La suma entre 5 y 3 </h2>' + calculadora.sumar(5, 3))
+})
 
 app.listen(8000 , ()=>{
     console.log('Ejecutando app')
